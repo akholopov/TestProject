601855b76daf8ed672029db94c6a43c51c5665ce
akholopov <akholopov.pl@gmail.com>
2018-02-05 22:43:28 +0100

    test

---
+++ src/test/java/testPtoject.java
@@ -0,0 +1,19 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class testPtoject {
+    @Test
+    public void googleTest() {
+        open("http://google.com");
+        $(By.cssSelector("input.gsfi"))
+                .setValue("williamhill")
+                .pressEnter();
+        Assertions.assertThat($(By.cssSelector("span._Xbe._ZWk.kno-fv"))
+                .getText())
+                .isEqualTo("00 800 3551 3551");
+    }
+}
